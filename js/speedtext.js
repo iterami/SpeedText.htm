0ccdd06046b3c83f4718389010c8cc4d41f2bb6a
Jan Horava <czechinthemail@gmail.com>
2014-10-23 17:34:32 -0700

    improved formatting and clarity

--- js/speedtext.js
+++ js/speedtext.js
@@ -4,7 +4,7 @@ function clicked(){
 }
 
 function clear_links(){
-    // Remove all target links from text
+    // Remove all target links from text.
     document.getElementById('text').innerHTML =
       document.getElementById('text').innerHTML.replace('<a onclick="clicked()">', '').replace('</a>', '');
 }
@@ -23,30 +23,33 @@ function generate(){
     clear_links();
     j = parseInt(document.getElementById('link-length').value);
 
-    // Generate a range of link-length that doesn't overwrite any HTML
+    // Generate a range of link-length that doesn't overwrite any HTML.
     do{
         i = Math.floor(Math.random() * (document.getElementById('text').innerHTML.length - j));
     }while(document.getElementById('text').innerHTML.substr(i, j).indexOf("<") !== -1
       || document.getElementById('text').innerHTML.substr(i, j).indexOf(">") != -1);
 
-    // Replace the text with the new target link in it
+    // Replace the text with the new target link in it.
     document.getElementById('text').innerHTML = document.getElementById('text').innerHTML.substr(0, i)
       + '<a onclick="clicked()">' + document.getElementById('text').innerHTML.substr(i, j) + '</a>'
       + document.getElementById('text').innerHTML.substr(i + j, document.getElementById('text').innerHTML.length - i);
 }
 
 function reset(){
-    if(confirm('Reset settings?')){
-        document.getElementById('audio-volume').value = 1;
-        document.getElementById('link-length').value = 5;
-        document.getElementById('start_button').value = 'Start [H]';
-        document.getElementById('start-key').value = 'H';
-
-        save();
+    if(!confirm('Reset settings?')){
+        return;
     }
+
+    document.getElementById('audio-volume').value = 1;
+    document.getElementById('link-length').value = 5;
+    document.getElementById('start_button').value = 'Start [H]';
+    document.getElementById('start-key').value = 'H';
+
+    save();
 }
 
 function save(){
+    // Save audio-volume setting.
     if(document.getElementById('audio-volume').value === 1
       || document.getElementById('audio-volume').value < 0){
         window.localStorage.removeItem('SpeedText.htm-audio-volume');
@@ -59,6 +62,7 @@ function save(){
         );
     }
 
+    // Save link-length setting.
     if(document.getElementById('link-length').value === 5
       || document.getElementById('link-length').value < 1){
         window.localStorage.removeItem('SpeedText.htm-link-length');
@@ -71,6 +75,7 @@ function save(){
         );
     }
 
+    // Save start-key setting.
     if(document.getElementById('start-key').value === 'H'){
         window.localStorage.removeItem('SpeedText.htm-start-key');
 
@@ -145,10 +150,11 @@ window.onkeydown = function(e){
     var key = window.event ? event : e;
     key = key.charCode ? key.charCode : key.keyCode;
 
-    if(key === 27){// ESC
+    // ESC: stop current game.
+    if(key === 27){
         stop();
 
-    }else if(String.fromCharCode(key) === document.getElementById('start-key').value){// H
+    }else if(String.fromCharCode(key) === document.getElementById('start-key').value){
         stop();
         start();
     }
