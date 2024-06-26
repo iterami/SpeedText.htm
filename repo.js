98ce3b7e2e5ec44c7dcb64e855fb5adb0e27c2f4
Jan Hořava <czechinthemail@gmail.com>
2024-06-26 14:15:44 -0700

    changed length input to step=1

--- repo.js
+++ repo.js
@@ -89,7 +89,7 @@ function repo_init(){
         'time-max': 30,
       },
       'storage-menu': '<textarea id=text></textarea>'
-        + '<table><tr><td><input class=mini id=link-length min=1 step=any type=number><td>Link Length'
+        + '<table><tr><td><input class=mini id=link-length min=1 step=1 type=number><td>Link Length'
         + '<tr><td><input class=mini id=time-max step=any type=number><td>Time</table>',
       'title': 'SpeedText.htm',
       'ui': 'Score: <span id=score></span><br>Time: <span id=time></span> / <span id=time-max-span>30</span><hr><div id=text-div></div>',
